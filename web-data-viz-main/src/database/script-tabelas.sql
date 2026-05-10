c683d748d83f73c94c3d33b70a4b9d10ee05be7a
mariaeduarda1430-ui <maria.lsilva@sptech.school>
2026-05-10 19:47:36 -0300

    finalização da conexao da api juntamente a adição do nome na tela inicial ao logar na pagina (uso de validação para caso entre na pagina sem o login ele seja redirecionado para ele)

--- web-data-viz-main/src/database/script-tabelas.sql
+++ web-data-viz-main/src/database/script-tabelas.sql
@@ -1,62 +1,35 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+create database proj_individual;
+use proj_individual;
 
-/*
-comandos para mysql server
-*/
-
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
+create table usuario (
+idUsuario int primary key auto_increment,
+nome varchar(45),
+email varchar(70),
+senha varchar(15)
 );
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+create table caracteristica (
+idcaracteristica int primary key auto_increment,
+raça varchar(45),
+porte varchar(45),
+cor varchar(45),
+especie varchar(45)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+create table animal (
+idAnimal int primary key auto_increment,
+tipo varchar(45),
+fkusuario int,
+fkcaracteristica int,
+constraint fkusuario_const foreign key (fkusuario) references usuario(idUsuario),
+constraint fkcaracteristica_const foreign key (fkcaracteristica) references caracteristica(idcaracteristica)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+create table Comida (
+idcomida int primary key auto_increment,
+peso decimal(4,2),
+comida varchar(45),
+dtcomida datetime,
+fkanimal int,
+constraint fkanimal_const foreign key (fkanimal ) references animal(idAnimal)
 );
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
--- web-data-viz-main/src/routes/index.js
+++ web-data-viz-main/src/routes/index.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var router = express.Router();
+var path = require("path");
 
 router.get("/", function (req, res) {
-    res.render("index");
+    res.sendFile(path.join(__dirname, "../../public/cadastro.html"));
 });
 
 module.exports = router;
